9d8bbfaafa331853ee8cfd4172c039eefd6734a0
Casper Dijkstra <cdijkstra@MacBook-Pro.local>
2020-09-07 16:14:17 +0200

    Moved backend to right folder

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-7109"
-    storage_account_name = "terraformbackend21106"
-    container_name       = "tfstates-network-3682"
-    key                  = "terraform.tfstate"
-  }
-}
