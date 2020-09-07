bd17dd588b23a09fa52f25dc8345255746a2e661
Casper Dijkstra <cdijkstra@MacBook-Pro.local>
2020-09-07 16:18:30 +0200

    New provider.tf

--- infra/dev/provider.tf
+++ infra/dev/provider.tf
@@ -1,6 +1,10 @@
-# Configure the Azure Provider
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=2.26"
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.25.0"
+    }
+  }
 }
+provider "azurerm" {
+  features {}
